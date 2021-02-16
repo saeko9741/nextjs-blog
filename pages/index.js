2dfcee69c11b29d150c3f1e69f45403b19e6270a
saeko9741 <saeko9741@gmail.com>
2021-02-16 14:57:45 +0900

    Add Image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -16,6 +17,13 @@ export default function Home() {
             <a>this page!</a>
           </Link>
         </h1>
+
+        <Image
+          src="/images/profile.jpg"
+          height={144}
+          width={144}
+          alt="Siida"
+        />
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
